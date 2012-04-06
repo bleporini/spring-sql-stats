79e5e8e3dd22fb99003a48e112d925bcabf86b13
Brice LEPORINI <bleporini@gmail.com>
2012-04-06 07:34:37 +0200

    Revert "Refactoring"
    This reverts commit bca777d6c9c49b678b08edb8df484ef2ff7787b5.

--- src/main/java/org/blep/spring/sql/stats/PreparedStatementWrapper.java
+++ src/main/java/org/blep/spring/sql/stats/PreparedStatementWrapper.java
@@ -1,10 +1,13 @@
 package org.blep.spring.sql.stats;
 
 import lombok.Delegate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 /**
  * @author blep
@@ -14,14 +17,47 @@ import java.sql.PreparedStatement;
 @Component
 @Scope("prototype")
 class PreparedStatementWrapper extends StatementWrapper implements PreparedStatement{
+    @Autowired
+    private StatController controller;
 
-    @Delegate
+
+    private interface ExcludeMethod {
+
+        ResultSet executeQuery() throws java.sql.SQLException;
+
+        int executeUpdate() throws java.sql.SQLException;
+
+        boolean execute() throws java.sql.SQLException;
+    }
+
+    @Delegate(excludes = {ExcludeMethod.class,StatementWrapper.class})
     private PreparedStatement delegate;
 
     PreparedStatementWrapper(PreparedStatement preparedStatement) {
         super(preparedStatement);
         delegate =  preparedStatement;
+
     }
 
+    @Override
+    public boolean execute() throws java.sql.SQLException{
+        controller.newQuery();
+        return delegate.execute();
+    }    
+
+
+    @Override
+    public ResultSet executeQuery() throws java.sql.SQLException{
+        controller.newQuery();
+        return delegate.executeQuery();
+    }
+
+
+
+    @Override
+    public int executeUpdate() throws java.sql.SQLException{
+        controller.newQuery();
+        return delegate.executeUpdate();
+    }
 
 }
--- src/main/java/org/blep/spring/sql/stats/StatController.java
+++ src/main/java/org/blep/spring/sql/stats/StatController.java
@@ -1,8 +1,5 @@
 package org.blep.spring.sql.stats;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -23,14 +20,6 @@ public class StatController implements StatControllerMBean {
     private long totalConnectionUsed=0;
     private long lastConnectionTiming = 0;
     private long averageConnectionTiming = 0;
-
-    @Getter @Setter @AllArgsConstructor
-    public static class SlowestSql{
-        private String sql;
-        private long duration;
-    }
-
-
 
     public void newConnection(){
         activeConnectionCount++;
